173c8e9e4aac2adb0b89b49f75795c45eca4ec4e
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2022-10-13 17:33:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /* C program to write a sentence */
-
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
