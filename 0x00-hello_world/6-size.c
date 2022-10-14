77ff3aa3a88646ddda6694b4a13d1f3ce5d1a1f1
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2022-10-13 17:54:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: expecting 0
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %i byte(s)\n", (int)sizeof(c));
+	printf("Size of an int: %i byte(s)\n", (int)sizeof(i));
+	printf("Size of a long int: %i byte(s)\n", (int)sizeof(li));
+	printf("Size of a long long int: %i byte(s)\n", (int)sizeof(lli));
+	printf("Size of a float: %i byte(s)\n", (int)sizeof(f));
+}
