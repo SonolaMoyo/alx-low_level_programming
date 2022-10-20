fe18f53a1fbe39fd879e4ce487c04fd18f325c39
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2022-10-19 19:22:30 -0700

    a program that prints

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	char h[9] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(h[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
