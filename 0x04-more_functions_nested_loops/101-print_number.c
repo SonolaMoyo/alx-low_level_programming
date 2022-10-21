803960c9adc2cc9374273ebcb9a0ce02f8f02667
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2022-10-20 20:33:52 -0700

    a function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - Function
+ * @n: Int
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n = -1 * n;
+		_putchar('-');
+	}
+	n1 = n;
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar(n1 % 10 + '0');
+}
