1ea75a1a18a361f44d05fd10c06945738eeef191
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2022-11-17 10:41:19 -0800

    alx - structure_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
+ * init_dog -initialize a variable of type struct dog
  * @d: Pointer to struct dog
  * @name: Pointer to string
  * @age: age of dog
@@ -11,9 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-    return;
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,64 +1,66 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _strlen(char *s);
+
 /**
  * new_dog - creates a new dog (struct dog)
  * @name: name of the new dog
- * @age: age of the new dog
+ * @age: age of new dog
  * @owner: owner of new dog
  * Return: pointer of new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i;
-dog_t *newDog;
+	int i;
+	dog_t *newDog;
 
-newDog = malloc(sizeof(dog_t));
-if (newDog == NULL)
-    return NULL;
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
 
-newDog->name = malloc(_strlen(name) + 1);
-if (newDog->name == NULL)
-{
-    free(newDog);
-    return (NULL);
-}
+	newDog->name = malloc(_strlen(name) + 1);
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
 
-newDog->owner = malloc(_strlen(owner) + 1);
-if (newDog->owner == NULL)
-{
-    free(newDog->name);
-    free(newDog);
-    return (NULL);
-}
+	newDog->owner = malloc(_strlen(owner) + 1);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
 
-for (i = 0; name[i] != '\0'; i++)
-    newDog->name[i] = name[i];
-newDog->name[i] = '\0';
+	for (i = 0; name[i] != '\0'; i++)
+		newDog->name[i] = name[i];
+	newDog->name[i] = '\0';
 
-for (i = 0; owner[i] != '\0'; i++)
-    newDog->owner[i] = owner[i];
-newDog->owner[i] = '\0';
+	for (i = 0; owner[i] != '\0'; i++)
+		newDog->owner[i] = owner[i];
+	newDog->owner[i] = '\0';
 
-newDog->age = age;
+	newDog->age = age;
 
-return (newDog);
+	return (newDog);
 }
 
 /**
- * _strlen - Calculate length of the string
+ * _strlen - Calculate length of string
  * @s: Pointer to string
  * Return: Length of string
  */
 int _strlen(char *s)
 {
-int i;
+	int i;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-}
-return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+
 }
